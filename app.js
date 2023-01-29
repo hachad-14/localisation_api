5d6b2c32a7761b8cd07fd9547991d45f10b6a35c
hachad-14 <hachadsaad14@gmail.com>
2023-01-29 11:36:33 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    const firebaseConfig = {
+        databaseURL: "https://database-ab4c8-default-rtdb.firebaseio.com",
+        storageBucket: "database-ab4c8.appspot.com",
+        measurementId: "G-12GPLK769J",
+        apiKey: "AIzaSyDmoMzJQSsXqClvaYkURj58jJw_vpX4640",
+        authDomain: "database-ab4c8.firebaseapp.com",
+        projectId: "database-ab4c8",
+        storageBucket: "database-ab4c8.appspot.com",
+        messagingSenderId: "85816447775",
+        appId: "1:85816447775:web:1b7fb8a2ccf7d88bd7962a"
+
+    };
+    firebase.initializeApp(firebaseConfig);
+
+});    
---
+++ localisation_api.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+    const ua = navigator.userAgent
+    const device = {
+      iPhone: /iPhone/.test(ua),
+      Linux: /Linux/.test(ua)
+    }
+    console.log(device);
+
+    function writeUserData(userId, name, email, imageUrl) {
+        firebase.database().ref('api/' + "localisation").set({
+          username: device,
+          exact_localisation : "imageUrl"
+        });
+    }
+
+    document.getElementById('upload').onclick = function() {
+        writeUserData();
+        console.log('uploaded!')
+    };
+
+    function get_Order() {
+      updateVisitCount();
+    
+      function updateVisitCount() {
+        fetch('https://api.countapi.xyz/update/posts/Order/?amount=-1')
+        .then(res => res.json())
+        .then(res => {
+          console.log(res.value);
+          order = res.value;
+        })
+      }
+    }
+});
+  
