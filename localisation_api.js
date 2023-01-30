462dd8b99f2586ed4cde5ecc432844a0b98076c0
hachad-14 <hachadsaad14@gmail.com>
2023-01-30 05:12:36 +0100

    test

--- localisation_api.js
+++ localisation_api.js
@@ -1,21 +1,23 @@
 $(document).ready(function() {
 
     const date = new Date();
+
     var device = deviceAPI.deviceName;
+    //var location = navigator.geolocation;
+    //const latitude  = position.coords.latitude;
+    //const longitude = position.coords.longitude;
 
 
-    const id = navigator.geolocation.watchPosition(successCallback, errorCallback);
     const successCallback = (position) => {
       alert(position);
     };
-    
     const errorCallback = (error) => {
       alert(error);
     };
-
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
     writeUserData();
-  
+
     document.getElementById('upload').onclick = function() {
       writeUserData();
       console.log('uploaded!');
