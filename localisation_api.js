531cdae842896d0f20827c88a5433b235229c3f5
hachad-14 <hachadsaad14@gmail.com>
2023-01-30 05:01:04 +0100

    test

--- localisation_api.js
+++ localisation_api.js
@@ -3,28 +3,27 @@ $(document).ready(function() {
     const date = new Date();
 
     var device = deviceAPI.deviceName;
-    //var location = navigator.geolocation;
-    //const latitude  = position.coords.latitude;
-    //const longitude = position.coords.longitude;
 
+    function locate() {
+      const successCallback = (position) => {
+        document.getElementById("console").innerHTML(position);
+      };
 
-    const successCallback = (position) => {
-      document.getElementById("console").innerHTML(position);
-    };
-    
-    const errorCallback = (error) => {
-      document.getElementById("console").innerHTML(error);
-    };
-    
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+      const errorCallback = (error) => {
+        document.getElementById("console").innerHTML(error);
+      };
+
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    }
 
     writeUserData();
 
     document.getElementById('upload').onclick = function() {
+      locate();
       writeUserData();
       console.log('uploaded!');
     };
-    
+
     function writeUserData() {
       firebase.database().ref("api/" + "localisation").set({
         Date: date + "",
